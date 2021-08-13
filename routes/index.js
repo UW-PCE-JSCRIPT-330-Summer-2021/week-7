1a44ac53c6be204d9331afa442f0392ed21e3bae
Surry <50641921+surryjm@users.noreply.github.com>
2021-08-13 16:42:57 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
+router.get('/', (req, res, next) => {
+  const timeStamp = new Date().toTimeString();
+  res.render('index', { data: {time: timeStamp }, items: ['item one', 'item two', 'item three'] });
+});
 
 module.exports = router;
