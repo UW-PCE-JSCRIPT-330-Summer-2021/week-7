5e4ece22c1e4cd960276cb78fff48b4d7d0aac4e
Eddie Lee <eddie@workbook.localdomain>
2021-08-17 15:22:54 -0700

    clean up

--- routes/weather.js
+++ routes/weather.js
@@ -4,20 +4,20 @@ const router = Router()
 const weatherDAO = require('../daos/weather');
 
 router.get("/", (req, res, next) => {
-  return res.render('weather');
+  return res.render('weather')
 });
 
 router.get("/location", async (req, res, next) => {
   const location = req.query.name;
   if (!location || location === '') {
-    return res.redirect(302, '/weather'); 
+    return res.redirect(302, '/weather');
   }
   try {
     const weather = await weatherDAO.getWeatherByLocation(location);
     if (!weather) {
-      return res.status(404).render('location', { name: location, temperature: 'not available' });
+      return res.status(404).render('location', { name: location, temperature: 'not available' })
     }
-    return res.render('location', { name: weather.name, temperature: weather.temperature });
+    return res.render('location', { name: weather.name, temperature: weather.temperature })
   } catch (e) {
     return res.status(500).send(e.message)
   }
